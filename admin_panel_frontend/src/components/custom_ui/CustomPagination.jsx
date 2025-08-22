829b007730a0669f11abb7f9e37637b160d29e86
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-22 19:23:53 +0530

    pagination responsive

--- admin_panel_frontend/src/components/custom_ui/CustomPagination.jsx
+++ admin_panel_frontend/src/components/custom_ui/CustomPagination.jsx
@@ -20,67 +20,87 @@ export const CustomPagination = ({ currentPage, totalPages, onPageChange }) => {
     }
   };
 
-  // Helper: get pages to display
-  const getPages = () => {
+  // Full version (desktop/tablet)
+  const getDesktopPages = () => {
     let pages = [];
-
-    // Always show first page
     pages.push(1);
 
-    // Show left ellipsis if currentPage > 3
-    if (currentPage > 3) {
-      pages.push("...");
-    }
-
-    // Show currentPage -1, currentPage, currentPage +1
+    if (currentPage > 3) pages.push("...");
     for (let i = currentPage - 1; i <= currentPage + 1; i++) {
-      if (i > 1 && i < totalPages) {
-        pages.push(i);
-      }
+      if (i > 1 && i < totalPages) pages.push(i);
     }
+    if (currentPage < totalPages - 2) pages.push("...");
+    if (totalPages > 1) pages.push(totalPages);
 
-    // Show right ellipsis if currentPage < totalPages - 2
-    if (currentPage < totalPages - 2) {
+    return pages;
+  };
+
+  // Simplified version (mobile)
+  const getMobilePages = () => {
+    let pages = [1];
+    if (currentPage !== 1 && currentPage !== totalPages) {
+      pages.push("...", currentPage, "...");
+    } else {
       pages.push("...");
     }
-
-    // Always show last page
-    if (totalPages > 1) {
-      pages.push(totalPages);
-    }
-
+    if (totalPages > 1) pages.push(totalPages);
     return pages;
   };
 
   return (
     <Pagination>
       <PaginationContent>
+        {/* Previous Arrow */}
         <PaginationItem>
           <PaginationPrevious
             onClick={handlePrevious}
             disabled={currentPage === 1}
+            className="px-2 sm:px-4" // smaller on mobile
           />
         </PaginationItem>
 
-        {getPages().map((page, idx) => (
-          <PaginationItem key={idx}>
-            {page === "..." ? (
-              <span className="px-2">...</span>
-            ) : (
-              <PaginationLink
-                isActive={currentPage === page}
-                onClick={() => onPageChange(page)}
-              >
-                {page}
-              </PaginationLink>
-            )}
-          </PaginationItem>
-        ))}
+        {/* Desktop Pagination */}
+        <div className="hidden sm:flex">
+          {getDesktopPages().map((page, idx) => (
+            <PaginationItem key={idx}>
+              {page === "..." ? (
+                <span className="px-2">...</span>
+              ) : (
+                <PaginationLink
+                  isActive={currentPage === page}
+                  onClick={() => onPageChange(page)}
+                >
+                  {page}
+                </PaginationLink>
+              )}
+            </PaginationItem>
+          ))}
+        </div>
 
+        {/* Mobile Pagination */}
+        <div className="flex sm:hidden">
+          {getMobilePages().map((page, idx) => (
+            <PaginationItem key={idx}>
+              {page === "..." ? (
+                <span className="px-2">...</span>
+              ) : (
+                <PaginationLink
+                  isActive={currentPage === page}
+                  onClick={() => onPageChange(page)}
+                >
+                  {page}
+                </PaginationLink>
+              )}
+            </PaginationItem>
+          ))}
+        </div>
+
+        {/* Next Arrow */}
         <PaginationItem>
           <PaginationNext
             onClick={handleNext}
             disabled={currentPage === totalPages}
+            className="px-2 sm:px-4"
           />
         </PaginationItem>
       </PaginationContent>
