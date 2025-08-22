f6a3b04a716432c48030417aac019ba793237ac4
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-22 17:24:17 +0530

    table removed

--- admin_panel_frontend/src/components/Role/RoleSection.jsx
+++ admin_panel_frontend/src/components/Role/RoleSection.jsx
@@ -21,53 +21,6 @@ export function RoleSection({ isExpanded }) {
         buttonText="Create New Role"
         tooltipText="Create New Role"
       />
-      {/* Modified container with proper constraints */}
-      <div className="mt-4 border border-border rounded-lg max-w-full">
-        <div className="overflow-x-auto">
-          <Table className="min-w-full">
-            <TableCaption>List of your all type of roles.</TableCaption>
-            <TableHeader className="bg-hoverBg">
-              <TableRow>
-                <TableHead className="w-[50px]">SR</TableHead>
-                <TableHead className="min-w-[100px]">Name</TableHead>
-                <TableHead className="min-w-[100px]">Actions</TableHead>
-                <TableHead className="min-w-[100px]">Logs</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell className="font-medium">1</TableCell>
-                <TableCell>Paid</TableCell>
-                <TableCell>
-                  <Button>Edit</Button>
-                </TableCell>
-                <TableCell>Credit Card</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">2</TableCell>
-                <TableCell>
-                  Administrator with a very long role name that might cause
-                  overflow
-                </TableCell>
-                <TableCell>
-                  <Button>Edit</Button>
-                </TableCell>
-                <TableCell>
-                  System logs, application logs, user activity logs
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">3</TableCell>
-                <TableCell>Editor</TableCell>
-                <TableCell>
-                  <Button>Edit</Button>
-                </TableCell>
-                <TableCell>Content modification logs</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </div>
-      </div>
     </div>
   );
 }
