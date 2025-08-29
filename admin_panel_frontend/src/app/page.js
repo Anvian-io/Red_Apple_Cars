855c0782323a5c96a4f3a1a22e7b40ad3206918b
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-30 00:45:05 +0530

    small change

--- admin_panel_frontend/src/app/page.js
+++ admin_panel_frontend/src/app/page.js
@@ -2,5 +2,5 @@ import { Navbar,Dashboard } from "@/components";
 import { Children } from "react";
 export default function Home({Children}) {
 
-  return <div className="min-h-screen bg-background"><Navbar>{Children}</Navbar></div>;
+  return <div className="min-h-screen bg-background">{Children}</div>;
 }
