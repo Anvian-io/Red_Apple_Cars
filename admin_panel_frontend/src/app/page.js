b49e246b70e771f65a919bff004472733ac57d54
suthakaranburaj <suthakaranburaj@gmail.com>
2025-09-07 21:17:34 +0530

    Dashboard Page done

--- admin_panel_frontend/src/app/page.js
+++ admin_panel_frontend/src/app/page.js
@@ -1,6 +1,11 @@
-import { Navbar,Dashboard } from "@/components";
+"use client"
+import { Navbar, Dashboard } from "@/components";
 import { Children } from "react";
-export default function Home({Children}) {
 
-  return <div className="min-h-screen bg-background">{Children}</div>;
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Dashboard />
+    </div>
+  );
 }
