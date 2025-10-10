2276c23d0089f04677f943e9fe58601d182c06e3
suthakaranburaj <suthakaranburaj@gmail.com>
2025-10-10 22:06:54 +0530

    small change

--- admin_panel_frontend/src/components/cars/CarsSection.jsx
+++ admin_panel_frontend/src/components/cars/CarsSection.jsx
@@ -704,7 +704,7 @@ export function CarSection({ isExpanded }) {
                       </TableCell>
                     )}
                     {columnVisibility.actions && (
-                      <TableCell className="text-right sticky right-0 bg-tableBg z-10">
+                      <TableCell className="text-right">
                         <div className="flex justify-end gap-2">
                           <Button
                             onClick={() => handleEditCar(car._id)}
